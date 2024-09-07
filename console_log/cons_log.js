7cef75e19ae445500df5f167fedaa87df01ab3b6
Ba1Kerrr <daniildavidov2707@gmail.com>
2024-09-07 20:09:47 +0300

    .

---
+++ console_log/cons_log.js
@@ -0,0 +1,7 @@
+let year = prompt("в каком году появился язык assembler",0);
+if (year < 1949) {
+  alert( 'Это слишком рано...' );}
+else if (year > 1949) {
+  alert( 'Это поздновато' );}
+ else {
+  alert( 'Верно!' );}
---
+++ using operators/ternary_operator.js
@@ -0,0 +1,2 @@
+let year = prompt("в каком году появился язык assembler"),answer = undefined;
+year == "1949" ? alert("True") : alert("False")
